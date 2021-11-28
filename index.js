5a1d0d3200c5451b7fc3be5d3ec595df97307a46
tankcool <xiaotong.cn@gmail.com>
2021-11-28 22:17:53 +0800

    feat: include journals setting panel

--- index.js
+++ index.js
@@ -1,29 +1,75 @@
-function createModel() {
-  return {
-    handleBtnClick() {
-      openRandomPage()
-    },
-  };
+import '@logseq/libs'
+
+import { h, render } from 'preact'
+import { useEffect, useState } from 'preact/hooks'
+
+
+function IncludeJournal(props) {
+  useEffect(() => {
+    let { includeJournal } = props
+    logseq.updateSettings({
+      randomNoteIncludeJournals: includeJournal
+    })
+  }, [props.includeJournal])
+
+  return (
+    <div className="rt">
+      <label><strong>Include Journals:</strong>
+        <input type="checkbox" 
+        name="includeJournal"
+        checked={props.includeJournal ? 'checked' : ''}
+        onChange={(e) => {
+          logseq.updateSettings({
+            randomNoteIncludeJournals: e.target.checked
+          })
+        }}
+        />
+        </label>
+    </div>
+  )
 }
 
-function openPage(pageName) {
-  logseq.App.pushState('page', {
-    name: pageName
-  })
+function App() {
+  const [settings, setSettings] = useState(logseq.settings)
+  useEffect(() => {
+    logseq.on('settings:changed', (a) => {
+      setSettings(a)
+    })
+  }, [])
+
+  return (
+    <div>
+      <IncludeJournal includeJournal={settings.randomNoteIncludeJournals}></IncludeJournal>
+      <p className="ctl">
+        <button onClick={() => {
+          logseq.hideMainUI()
+        }}>Close Settings
+        </button>
+      </p>
+    </div>
+  )
 }
 
-async function openRandomPage() {
-  try {
-    let ret = await logseq.DB.datascriptQuery(`
+async function openRandomNote() {
+  var query = `
+  [:find (pull ?p [*])
+    :where
+    [_ :block/page ?p]
+    [?p :block/journal? false]]`
+  if (logseq.settings.randomNoteIncludeJournals) {
+    query = `
     [:find (pull ?p [*])
       :where
-      [_ :block/page ?p]
-      [?p :block/journal? false]]
-    `)
+      [_ :block/page ?p]]` 
+  }
+  try {
+    let ret = await logseq.DB.datascriptQuery(query)
     const pages = ret?.flat()
     if(pages && pages.length > 0) {
       const index = Math.floor(Math.random() * pages.length)
-      openPage(pages[index].name)
+      logseq.App.pushState('page', {
+        name: pages[index].name
+      })
     }
   } catch (err) {
     console.log(err)
@@ -31,41 +77,64 @@ async function openRandomPage() {
 }
 
 function main() {
-  logseq.setMainUIInlineStyle({
-    position: "fixed",
-    zIndex: 11,
-  });
-  const key = logseq.baseInfo.id;
-  const btnName = "random-note";
-  // add style
+
+  const doc = document
+
+  render(<App/>, doc.querySelector('#app'))
+
+  logseq.provideModel({
+    openSettingPanel (e) {
+      const { rect } = e
+      logseq.setMainUIInlineStyle({
+        top: `${rect.top + 25}px`,
+        left: `${rect.right - 17}px`,
+      })
+      logseq.toggleMainUI()
+    },
+    handleRandomNote() {
+      openRandomNote()
+    }
+  },
+)
+
   logseq.provideStyle(`
-  @import url("https://at.alicdn.com/t/font_2809512_xxdycbg5pv.css");
-  div[data-injected-ui=${btnName}-${key}] {
+  @import url("https://at.alicdn.com/t/font_2809512_lswgn9yir8.css");
+  
+  .logseq-random-note-toolbar {
     display: flex;
-    align-items: center;
-    font-weight: 500;
-    position: relative;
-    top: 0px;
+    border-radius: 5px;
   }
-  
-  div[data-injected-ui=${btnName}-${key}] a {
-    opacity: 1;
-    padding: 6px;
+
+  .logseq-random-note-toolbar a.button {
+    padding: 0;   
+    margin: 0;
   }
-  
-  div[data-injected-ui=${btnName}-${key}] iconfont {
+
+  .logseq-random-note-toolbar .iconfont {
     font-size: 18px;
   }
   `);
-  // add toolbar
+
+  logseq.setMainUIInlineStyle({
+    position: "fixed",
+    width: '290px',
+    zIndex: 999,
+    transform: 'translateX(-50%)',
+  });
+
   logseq.App.registerUIItem("toolbar", {
-    key: btnName,
+    key: 'logseq-random-note-toolbar',
     template: `
-      <a title="I'm Feeling Lucky" class="button" data-on-click="handleBtnClick">
-        <i class="iconfont icon-random"></i>
-      </a>
+      <span class="logseq-random-note-toolbar">
+        <a title="I'm Feeling Lucky" class="button" data-on-click="handleRandomNote">
+          <i class="iconfont icon-random"></i>
+        </a>
+        <a title="Random Note Settings" class="button" data-on-click="openSettingPanel" data-rect>
+          <i class="iconfont icon-shezhi"></i>
+        </a> 
+      </span>
     `,
   });
 }
 
-logseq.ready(createModel(), main).catch(console.error)
+logseq.ready(main).catch(console.error)
