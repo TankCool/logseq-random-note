cd17f1523caadfc0e3328cf0b90c2b0dc5af209d
tankcool <xiaotong.cn@gmail.com>
2023-04-30 15:43:39 +0800

    add telegram bot

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import "@logseq/libs";
+import axios from "axios";
 
 const settingsTemplate = [
-
   {
     key: "randomMode",
     type: "enum",
@@ -30,18 +30,59 @@ const settingsTemplate = [
     type: "string",
     default: "",
     title: "Query mode",
+    inputAs: "textarea",
     description:
       'Your custom query. e.g. [:find (pull ?b [*]) :where [?b :block/refs ?bp] [?bp :block/name "book"]]',
   },
   {
     key: "randomStepSize",
-    type: 'enum',
+    type: "enum",
     default: "1",
     title: "Random walk step size.",
-    description: "Random walk step size. Use it with caution. One shows in main area, others show in the right sidebar.",
+    description:
+      "Random walk step size. Use it with caution. One shows in main area, others show in the right sidebar.",
     enumChoices: ["1", "3", "5", "7", "10"],
-    enumPicker: 'radio'
-  }
+    enumPicker: "radio",
+  },
+  {
+    key: "enableTelegramBot",
+    title: "Enable telegram bot",
+    description: "Enable telegram bot. It doesn't work in page mode.",
+    type: "boolean",
+    default: false,
+  },
+  {
+    key: "telegramBotToken",
+    type: "string",
+    default: "",
+    title: "Telegram bot token",
+    description: "",
+  },
+  {
+    key: "telegramBotChatId",
+    type: "string",
+    default: "",
+    title: "Telegram bot chatId",
+    description: "",
+  },
+  {
+    key: "activeHours",
+    title: "Telegram bot active hours",
+    type: "enum",
+    enumChoices: Array.from({ length: 24 }, (_, i) => i + ""),
+    enumPicker: "checkbox",
+    default: [],
+    description: "",
+  },
+  {
+    key: "activeMinutes",
+    title: "Telegram bot active minutes",
+    type: "enum",
+    enumChoices: Array.from({ length: 60 }, (_, i) => i + ""),
+    enumPicker: "checkbox",
+    default: ["0"],
+    description: "",
+  },
 ];
 
 logseq.useSettingsSchema(settingsTemplate);
@@ -63,7 +104,7 @@ async function openRandomNote() {
       openRandomNoteInSidebar(pages, stepSize - 1);
     }
   } catch (err) {
-    logseq.App.showMsg(
+    logseq.UI.showMsg(
       err.message || "Maybe something wrong with the query",
       "error"
     );
@@ -96,10 +137,36 @@ async function openRandomNoteInMain(pages) {
  * @param {*} counts
  */
 async function openRandomNoteInSidebar(pages, counts) {
-  for(var i = 0; i < counts; i++) {
+  for (var i = 0; i < counts; i++) {
     const index = Math.floor(Math.random() * pages.length);
     const page = pages[index];
-    logseq.Editor.openInRightSidebar(page.uuid)
+    logseq.Editor.openInRightSidebar(page.uuid);
+  }
+}
+
+/**
+ * 发送消息到telegram bot
+ * @param {*} msg
+ */
+async function sendToTelegramBot(msg) {
+  const token = logseq.settings.telegramBotToken;
+  const chatId = logseq.settings.telegramBotChatId;
+  const sendUrl =
+    "https://api.telegram.org/bot" +
+    token +
+    "/sendMessage?chat_id=" +
+    chatId +
+    "&text=" +
+    encodeURIComponent(msg);
+  if (token && chatId) {
+    axios
+      .get(sendUrl)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 }
 
@@ -126,9 +193,11 @@ function getQueryScript() {
           [?p :block/journal? false]]`;
       }
     case "tags":
-      const tags = randomTags.map((item) => '"' + item.toLowerCase() + '"').join(",");
+      const tags = randomTags
+        .map((item) => '"' + item.toLowerCase() + '"')
+        .join(",");
       if (!logseq.settings.randomTags) {
-        logseq.App.showMsg("Random tags are required.", "warning");
+        logseq.UI.showMsg("Random tags are required.", "warning");
       }
       return (
         `
@@ -141,16 +210,14 @@ function getQueryScript() {
         `} ?name)]]
       `
       );
-      case "card":
-        return (
-        `
+    case "card":
+      return `
         [:find (pull ?b [*])
           :where
           [?b :block/refs ?bp]
           [?bp :block/name ?name]
           [(contains? #{"card"} ?name)]]
-        `
-        );
+        `;
     case "query":
       return logseq.settings.advancedQuery;
     default:
@@ -158,6 +225,40 @@ function getQueryScript() {
       return defaultQuery;
   }
 }
+
+async function getRandomNoteInfo() {
+  const queryScript = getQueryScript();
+  const ret = await logseq.DB.datascriptQuery(queryScript);
+  const blocks = ret?.flat();
+  if (blocks && blocks.length > 0) {
+    const index = Math.floor(Math.random() * blocks.length);
+    const randomNoteInfo = await getBlockReadableContent(blocks[index].uuid);
+    console.log("randomNoteInfo=" + randomNoteInfo);
+    return randomNoteInfo;
+  }
+  return "";
+}
+
+async function getBlockReadableContent(uid) {
+  const blockInfo = await logseq.Editor.getBlock(uid);
+  console.log(blockInfo);
+  console.log("before replace =>", blockInfo?.content);
+  const content =
+    blockInfo?.content.split(/\n.*::/)[0].replace("/\n.*::/", "") || "";
+  console.log("after replace =>", content);
+  if (hasRefUuid(content)) {
+    const parts = content.split("))");
+    const childContent = await getBlockReadableContent(
+      parts[0].replace("((", "")
+    );
+    return childContent + parts[1];
+  }
+  return content;
+}
+
+const hasRefUuid = (content) => {
+  return !!content && content.indexOf("((") > -1 && content.indexOf("))") > -1;
+};
 
 function main() {
   logseq.provideModel({
@@ -227,6 +328,48 @@ function main() {
       logseq.updateSettings({ randomMode: "query" });
     }
   );
+
+  logseq.App.registerCommandPalette(
+    {
+      key: "logseq-random-note:toggle-telegram-bot",
+      label: "Random note => Toggle Telegram Bot",
+    },
+    () => {
+      const enable = logseq.settings.enableTelegramBot;
+      logseq.updateSettings({ enableTelegramBot: !enable });
+      logseq.UI.showMsg(
+        "RandomNote Telegram Bot " + (enable ? "Disabled" : "Enabled"),
+        "success"
+      );
+    }
+  );
+
+  const sendContentInterval = setInterval(() => {
+    console.log("sendContentInterval running...");
+    const now = new Date();
+    const hour = now.getHours();
+    const minute = now.getMinutes();
+    console.log("hour: " + hour + " minute: " + minute);
+    const activeHours = logseq.settings.activeHours;
+    const activeMinutes = logseq.settings.activeMinutes;
+    const enableTelegramBot = logseq.settings.enableTelegramBot;
+    if (
+      enableTelegramBot &&
+      activeHours.indexOf(hour + "") > -1 &&
+      activeMinutes.indexOf(minute + "") > -1
+    ) {
+      getRandomNoteInfo().then((content) => {
+        console.log(content);
+        !!content && sendToTelegramBot(content);
+      });
+    }
+  }, 60 * 1000);
+
+  logseq.beforeunload(() => {
+    window.clearInterval(sendContentInterval);
+    console.log("logseq.beforeunload => clear sendContentInterval");
+    return Promise.resolve();
+  });
 }
 
 logseq.ready(main).catch(console.error);
